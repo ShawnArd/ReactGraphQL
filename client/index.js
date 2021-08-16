37b2e67dfa45976dda5b7f27bd2bd539be1168bc
Shawn Ard <shawnduncanard@gmail.com>
2021-08-16 12:50:28 -0400

    React front end input

--- client/index.js
+++ client/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { Router,Route, IndexRoute, hashHistory } from 'react-router';
+
+import App from "./components/App"
+import SongList from './components/songlist';
+import SongCreate from './components/SongCreate';
+
 
 const client = new ApolloClient({})
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div> Lyrical</div>
+      <Router history={hashHistory}>
+        <Route path="/"component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} /> 
+          </Route>
+      </Router>
     </ApolloProvider>
   )
 };
