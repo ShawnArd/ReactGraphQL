a625343232ef095944765271093d3864629d7878
Shawn Ard <shawnduncanard@gmail.com>
2021-08-16 20:57:16 -0400

    deletion start

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import gql from "graphql-tag";
 import { graphql } from 'react-apollo'
 import {Link, hashHistory} from 'react-router'
+import  query  from '../queries/fetchSongs'
 
 class SongCreate extends Component {
 constructor(props) {
@@ -14,10 +15,9 @@ onSubmit(event) {
     event.preventDefault();
 
     this.props.mutate({
-        variables: {
-            title: this.state.title
-        }
-    }).then(() => hasHistory.push('/'))
+        variables: { title: this.state.title },
+        refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'))
 }
     render() {
         return (
